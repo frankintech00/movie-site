13e8f004ed41ed51f1f80f3111784a6beb05126d
Frank Kelly <frankintech00@gmail.com>
2023-01-24 12:50:11 +0000

    added comments

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 
+// default image to use if the movie poster is not available
 const DEFAULT_PLACEHOLDER_IMAGE =
   "https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie-1-1-696x1024.jpg";
 
+// Movie component that takes in a prop "movie" which is an object
 const Movie = ({ movie }) => {
+  // check if the movie poster is available, if not use the default image
   const poster =
     movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
+
   return (
+    // container element for the movie information
     <div className="text-center">
       <div className="">
+        {/* movie title */}
         <h2 className="text-gray-100 text-3xl my-3">{movie.Title}</h2>
+        {/* Movie image */}
         <img
           className="w-3/4 h-auto mx-auto"
           alt={`The movie titled: ${movie.Title}`}
           src={poster}
         />
       </div>
+      {/* movie year */}
       <p className="text-right text-xl text-gray-100 my-1 mr-12">
         ({movie.Year})
       </p>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,39 +1,45 @@
 import React, { useState } from "react";
 
+// Search component that takes in a prop "search" which is a function
 const Search = (props) => {
+  // useState hook to keep track of the current value of the search input
   const [searchValue, setSearchValue] = useState("");
 
+  // function to update the searchValue state when the input value changes
   const handleSearchInputChanges = (e) => {
     setSearchValue(e.target.value);
   };
 
+  // function to reset the searchValue state to an empty string
   const resetInputField = () => {
     setSearchValue("");
   };
 
+  // function to call the "search" prop function with the current searchValue and reset the input field
   const callSearchFunction = (e) => {
-    e.preventDefault();
-    props.search(searchValue);
-    resetInputField();
+    e.preventDefault(); // prevent the form from refreshing the page
+    props.search(searchValue); // call the "search" prop function with the current searchValue
+    resetInputField(); // reset the searchValue state
   };
 
   return (
+    // form element with a search input and a search button
     <form className="search bg-gray-700 w-full flex justify-center py-5">
       <div className="relative">
         <input
-          value={searchValue}
-          onChange={handleSearchInputChanges}
-          placeholder="Search for a movie..."
+          value={searchValue} // set the input value to the current searchValue state
+          onChange={handleSearchInputChanges} // call the handleSearchInputChanges function when the input value changes
+          placeholder="Search for a movie..." // input placeholder text
           type="text"
           className="inline-block block w-50% px-3 py-2 mr-5 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
           onKeyPress={(e) => {
             if (e.key === "Enter") {
-              callSearchFunction();
+              callSearchFunction(); // call the callSearchFunction function when the enter key is pressed
             }
           }}
         />
         <button
-          onClick={callSearchFunction}
+          onClick={callSearchFunction} // call the callSearchFunction function when the button is clicked
           className="inline-block ml-auto bg-emerald-700 text-white px-4 py-2 rounded-md hover:bg-teal-700 hover:text-white"
         >
           SEARCH
